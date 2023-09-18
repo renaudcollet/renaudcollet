53ff736abaddb4d7e05ee2896f943d25496a84b0
Renaud COLLET <collet.renaud@gmail.com>
2023-09-18 18:38:15 +0200

    Add Lenis

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,14 +10,11 @@
 </template>
 
 <script setup>
-import { onMounted, onUnmounted, watch, ref, computed } from "vue";
 import { useDatasStore, S_DATA_SEO } from '~/stores/datas';
+import Lenis from '@studio-freight/lenis';
 // import gsap from 'gsap';
 
-// import Lenis from '@studio-freight/lenis';
 // import Cover3D from '~/components/webgl/Cover3D.vue'
-// import utilsDevice from '~~/mixins/utils-device.js';
-
 
 // I don't know why, I can't use useHead() after receiving the data from the store
 // -> Can't useHead in async function
@@ -33,8 +30,10 @@ await fetchDatas(S_DATA_SEO);
 const datasSEO = storeDatas.seo.data.attributes;
 console.log('datasSEO', datasSEO);
 
+let lenis;
 let scrollVelocity = 0;
-let config = false
+let lastScroll = 0;
+let config = false;
 // let showScrollArrow = true;
 
 const route = useRoute()
@@ -42,6 +41,16 @@ const route = useRoute()
 const currentPage = computed(() => {
   return `page-${route.name}`
 })
+
+const update = time => {
+  lenis.raf(time)
+  scrollVelocity = lerp(scrollVelocity, 0, 0.2)
+  requestAnimationFrame(update)
+}
+
+const lerp = (a, b, n) => {
+  return (1 - n) * a + n * b;
+}
 
 watch(route, (to, from) => {
   // console.log('default layout - route changed', to, from)
@@ -54,16 +63,51 @@ watch(route, (to, from) => {
   )
     
   // showScrollArrow = route.name === 'index'
-  // lenis.scrollTo(0, 0)
+  lenis.scrollTo(0, 0)
 })
 
 onMounted(() => {
   config = window.location.hash === '#config'
+
+  const c1 = 1.70158;
+  const c3 = c1 + 1;
+  lenis = new Lenis({
+    duration: 1,
+    easing: (x) => {
+      // https://easings.net/
+      // return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
+      // return 1 + c3 * (x - 1) * (x - 1) * (x - 1) + c1 * (x - 1) * (x - 1);
+      // return 1 - Math.pow(1 - x, 3);
+      return 1 - (1 - x) * (1 - x) * (1 - x);
+    },
+    direction: 'vertical',
+    gestureDirection: 'vertical',
+    smooth: true,
+    mouseMultiplier: 0.5, // This is not in the doc, but it sets the wheelMultiplier
+    wheelMultiplier: 0.5, // if mouseMultiplier is not set, then wheelMultiplier is used
+    // normalizeWheel: true,
+    smoothTouch: false,
+    touchMultiplier: 0.5,
+  })
+  // Make Lenis available in every component
+  window.lenis = lenis
+  lastScroll = lenis.scroll
+
+  window.addEventListener('scroll', () => {
+    scrollVelocity = lenis.scroll - lastScroll
+    lastScroll = lenis.scroll
+  })
+
+  requestAnimationFrame(update)
+})
+
+onUnmounted(() => {
+  // clearScrollOpacity()
+  console.log('onUnmounted default layout');
 })
 </script>
 
 <script>
-// import Lenis from '@studio-freight/lenis';
 // // import Cover3D from '~/components/webgl/Cover3D.vue'
 // import utilsDevice from '~~/mixins/utils-device.js';
 // import { useDatasStore } from '~/stores/datas';
